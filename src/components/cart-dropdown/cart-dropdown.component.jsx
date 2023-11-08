324fb57cbeb5be7516c5e9ac701083b3dbeaed01
Zach Johnson <zgjohnson94@gmail.com>
2023-11-08 17:53:28 -0600

    cart item total increment

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
-import Button from '../button/button.component'
-import './cart-dropdown.styles.scss'
+import { useContext } from 'react';
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
+import { CartContext } from '../../context/cart.context';
 
 const CartDropdown = () => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
